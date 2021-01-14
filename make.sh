531dc0c73650510dc69393cd7775b9fba1c4ff30
Michele Santucci <weirdgyn@gmail.com>
2021-01-14 10:26:13 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 rm -f LeptonVid.o LeptonVid
 gcc -O2 -Wall -W -c LeptonVid.c -o LeptonVid.o
-gcc -W -O1 LeptonVid.o -LleptonSDKEmb32PUB/Debug -lLEPTON_SDK -lwiringPi -o LeptonVid
+gcc -W -O1 LeptonVid.o -LleptonSDKEmb32OEM/Debug -lLEPTON_SDK -lwiringPi -o LeptonVid
